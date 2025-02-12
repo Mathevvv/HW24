5cc4dff71d2f4ccf73ee39cb5e230c48da31256d
matthew <matvey.karasev.00@gmail.com>
2025-02-12 17:59:11 +0300

    Hard version of HW24 was completed

--- HW24/src/Main.java
+++ HW24/src/Main.java
@@ -1,9 +1,7 @@
-import java.util.Map;
-import java.util.Scanner;
-import java.util.TreeMap;
+import java.util.*;
 
 public class Main {
-    public static Map<String, String> phoneNumbers = new TreeMap<>();
+    public static Map<String, Set<String>> phoneNumbers = new TreeMap<>();
     private final static String NAME_REGEX = "[a-zA-Zа-яА-ЯёЁ]+";
 
     public static void main(String[] args) {
@@ -17,52 +15,75 @@ public class Main {
             } else if (input.equals("EXIT")) {
                 return;
             } else if (input.matches(NAME_REGEX)) {
-                nameRecording(input);
+                addByName(input);
             } else if (input.replaceAll("\\D", "").length() == 11) {
-                numberRecording(input);
+                addByNum(input);
             } else System.out.println("Ваш ввод некорректен");
         }
     }
 
-    private static void numberRecording(String input) {
-        if (!phoneNumbers.containsValue(input)) {
-        System.out.println("Запишите имя абонента для номера " + input);
+
+
+    private static void addByName(String input) {
+        if (phoneNumbers.containsKey(input)) System.out.println("Абонент с именем " + input + " уже есть в списке");
+        System.out.println("Введите номер абонента " + input + ":");
         String sinput = new Scanner(System.in).nextLine();
-        if (sinput.matches(NAME_REGEX)) {
-            if (!phoneNumbers.containsKey(sinput)) {
-                phoneNumbers.put(sinput, input);
-                System.out.println("Абонент " + sinput + " успешно добавлен с номером " + input);
-            }
-            else System.out.println("Этот абонент уже занесен с другим номером");
+        sinput = sinput.replaceAll("\\D+", "");
+        if (sinput.length()!=11) {
+            System.out.println("Это не номер!");
+            return;
         }
-        else System.out.println("Имя внесено некорректно");
-    }
-    else {
-        System.out.println("Номер уже есть у другого абонента");
-    }
+
+        for(Map.Entry<String, Set<String>> contact : phoneNumbers.entrySet()){
+            if (contact.getValue().contains(sinput)){
+                System.out.println("Номер " +  sinput + " уже есть у другого абонента");
+                return;
+            }
+        }
+        addToBook(input, sinput);
     }
 
-    private static void nameRecording(String input) {
-        if (!phoneNumbers.containsKey(input)){
-            System.out.println("Запишите номер абонента с именем " + input );
-            String sinput = new Scanner(System.in).nextLine();
-            if (sinput.replaceAll("\\D", "").length() == 11){
-                sinput = sinput.replaceAll("\\D", "");
-                if (!phoneNumbers.containsValue(sinput)) {
-                    phoneNumbers.put(input, sinput);
-                    System.out.println("Абонент " + input + " успешно добавлен с номером " + sinput);
-                }
-                else System.out.println("Этот номер уже есть у другого абонента");
+    private static void addByNum(String input) {
+
+        input = input.replaceAll("\\D+", "");
+        for(Map.Entry<String, Set<String>> contact : phoneNumbers.entrySet()){
+            if (contact.getValue().contains(input)){
+                System.out.println("Номер " +  input + " уже есть у другого абонента");
+                return;
             }
-            else System.out.println("Номер внесен некорректно");
         }
-        else System.out.println("Абонент " + input + " уже есть в списке");
+        System.out.println("Введите имя абонента для номера " + input + ":");
+        String sinput = new Scanner(System.in).nextLine();
+        if (!sinput.matches(NAME_REGEX)) {
+            System.out.println("Это не имя!");
+            return;
+        }
+        addToBook(sinput, input);
+
     }
+
 
 
     public static void print(){
-        for (Map.Entry<String, String> entry : phoneNumbers.entrySet()) {
-            System.out.println(entry.getKey() + " : " + entry.getValue());
+        for (Map.Entry<String, Set<String>> entry : phoneNumbers.entrySet()) {
+            System.out.println("Абонент: " + entry.getKey() + " : " );
+            for (String num: entry.getValue()) {
+                System.out.println("\t" + num);
+            }
         }
     }
+
+    public static void addToBook(String input, String sinput) {
+        if (phoneNumbers.containsKey(input)) {
+            phoneNumbers.get(input).add(sinput);
+            System.out.println("Абоненту " + input + " добавлен номер " + sinput);
+        }
+        else {
+            Set<String> numbers = new TreeSet<>();
+            numbers.add(sinput);
+            phoneNumbers.put(input, numbers);
+            System.out.println("Абонент " + input + " с номером " + sinput + " успешно записан в список");
+        }
+    }
+
 }
